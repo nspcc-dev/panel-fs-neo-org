a17f9d7724cf5eac87b132d9c0bea4fa0e23c1fa
Mikhail Petrov <mike@nspcc.ru>
2022-07-29 18:27:59 +0300

    [#1] Add index.js & style fixes
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { WalletConnectContextProvider } from "@cityofzion/wallet-connect-sdk-react";
+import { BrowserRouter } from "react-router-dom";
+
+const wcOptions = {
+  chains: ["neo3:testnet", "neo3:mainnet"],
+  logger: "debug",
+  methods: ["invokeFunction", "testInvoke", "signMessage", "verifyMessage"],
+  relayServer: "wss://relay.walletconnect.org",
+  appMetadata: {
+    name: "NeoFS Panel",
+    description: "NeoFS Panel",
+    url: process.env.REACT_APP_URL,
+    icons: [`${process.env.REACT_APP_URL}/img/favicon.ico`],
+  }
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WalletConnectContextProvider options={wcOptions}>
+			<BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </WalletConnectContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
