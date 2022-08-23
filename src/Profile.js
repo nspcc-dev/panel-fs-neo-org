e17d79cec10d98ec02e47e692b28560a251e9d2c
Mikhail Petrov <mike@nspcc.ru>
2022-08-23 18:56:35 +0300

    [#16] Move container to ContainerItem component
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- src/Profile.js
+++ src/Profile.js
@@ -9,9 +9,8 @@ import {
 	Button,
 	Box,
 	Tag,
-	Tabs,
 } from 'react-bulma-components';
-import TreeView from './Components/TreeView/TreeView';
+import ContainerItem from './Components/ContainerItem/ContainerItem';
 import api from './api';
 
 function formatForTreeView(objects) {
@@ -111,8 +110,6 @@ const Profile = ({
 	const [depositQuantity, setDepositQuantity] = useState(0);
 	const [withdrawQuantity, setWithdrawQuantity] = useState(0);
 
-	const [activeAttributes, setActiveAttributes] = useState('main');
-
 	useEffect(() => {
 		if (isLoadContainers) {
 			onGetContainers();
@@ -144,8 +141,6 @@ const Profile = ({
 	const onGetContainers = () => {
 		setIsLoadingContainers(true);
 		api('GET', `/containers?ownerId=${walletData.account}`).then((e) => {
-			const containersTemp = [ ...e.containers ];
-			containersTemp.map((container) => container.isActive = false);
 			setContainers(e.containers);
 			onPopup();
 			setTimeout(() => {
@@ -427,170 +422,17 @@ const Profile = ({
 							/>
 						</Heading>
 						{containers.map((containerItem, index) => (
-							<Tile
-								kind="ancestor"
+							<ContainerItem
 								key={containerItem.containerId}
-							>
-								<Tile kind="parent">
-									<Tile
-										kind="child"
-										renderAs={Notification}
-										color="gray"
-									>
-										<Heading
-											size={5}
-											weight="semibold"
-											style={{
-												cursor: 'pointer',
-												display: 'flex',
-												justifyContent: 'space-between',
-												alignItems: 'center',
-											}}
-											className={containerItem.isActive ? 'active' : ''}
-											onClick={() => {
-												const containersTemp = [ ...containers ];
-												containersTemp[index].isActive = !containerItem.isActive;
-												setContainers(containersTemp);
-												if (walletData.tokens.object.GET && containersTemp[index].isActive) {
-													onGetObjectData(containerItem.containerId, index);
-												}
-											}}
-										>
-											{formatForContainerName(containerItem.attributes, containerItem.containerId)}
-											{containerItem.isActive && (
-												<img
-													src="./img/trashbin.svg"
-													width={30}
-													height={30}
-													fill="#f14668"
-													alt="delete"
-													style={{ cursor: 'pointer' }}
-													onClick={(e) => {
-														onPopup('deleteContainer', { containerId: containerItem.containerId });
-														e.stopPropagation();
-													}}
-												/>
-											)}
-										</Heading>
-										{containerItem.isActive === true && (
-											<div className="container_panel">
-												{containerItem.ownerId ? (
-													<div>
-														<Section>
-															<Heading size={5} weight="bolder">Information</Heading>
-															<Heading size={6} weight="light">
-																<span>{`Container id: `}</span>
-																{containerItem.containerId}
-															</Heading>
-															<Heading size={6} weight="light">
-																<span>{`Owner id: `}</span>
-																{containerItem.ownerId}
-															</Heading>
-															<Heading size={6} weight="light">
-																<span>{`Placement policy: `}</span>
-																{containerItem.placementPolicy}
-															</Heading>
-															<Heading size={6} weight="light">
-																<span>{`Version: `}</span>
-																{containerItem.version}
-															</Heading>
-															<Heading size={6} weight="light">
-																<span>{`Basic acl: `}</span>
-																{`0x${containerItem.basicAcl}`}
-															</Heading>
-															<Button
-																color="primary"
-																onClick={() => onSetEacl(containerItem.containerId)}
-																style={{ margin: '10px 0', display: 'block' }}
-															>Set eACL</Button>
-														</Section>
-														<Section>
-															<Heading size={5} weight="bolder">Attributes</Heading>
-															<Tabs>
-																<Tabs.Tab
-																	onClick={() => {
-																		setActiveAttributes('main');
-																	}}
-																	active={activeAttributes === 'main'}
-																>
-																	Main
-																</Tabs.Tab>
-																<Tabs.Tab
-																	onClick={() => {
-																		setActiveAttributes('system');
-																	}}
-																	active={activeAttributes === 'system'}
-																>
-																	System
-																</Tabs.Tab>
-															</Tabs>
-															{activeAttributes === 'main' && containerItem.attributes.map((attribute) => ( attribute.key.indexOf('__NEOFS__') === -1 && (
-																<Heading size={6} weight="light" style={{ marginLeft: 10 }} key={attribute.key}>
-																	<span>{`${attribute.key === 'Timestamp' ? 'Creation' : attribute.key}: `}</span>
-																	{attribute.key === 'Timestamp' ? new Date(attribute.value * 1000).toLocaleDateString() : attribute.value}
-																</Heading>
-															)))}
-															{activeAttributes === 'system' && containerItem.attributes.map((attribute) => ( attribute.key.indexOf('__NEOFS__') !== -1 && (
-																<Heading size={6} weight="light" style={{ marginLeft: 10 }} key={attribute.key}>
-																	<span>{`${attribute.key === 'Timestamp' ? 'Creation' : attribute.key}: `}</span>
-																	{attribute.key === 'Timestamp' ? new Date(attribute.value * 1000).toLocaleDateString() : attribute.value}
-																</Heading>
-															)))}
-														</Section>
-														<Section>
-															<Heading size={5} weight="bolder">Objects</Heading>
-															<Box
-																style={{
-																	marginTop: 15,
-																	border: '1px solid #dbdbdc',
-																	boxShadow: '0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0 0 1px rgb(10 10 10 / 2%)',
-																}}
-															>
-																{walletData.tokens.object.GET ? (
-																	<>
-																		<TreeView
-																			walletData={walletData}
-																			onPopup={onPopup}
-																			containerIndex={index}
-																			containerItem={containerItem}
-																		/>
-																		<Button
-																			color="primary"
-																			onClick={() => onPopup('createObject', { containerId: containerItem.containerId })}
-																			style={{ display: 'flex', margin: '20px auto 0' }}
-																		>
-																			New object
-																		</Button>
-																	</>
-																) : (
-																	<div className="token_status_panel">
-																		<div>For getting operations</div>
-																		<Button
-																			color="primary"
-																			size="small"
-																			onClick={() => onAuth('object', 'GET')}
-																		>
-																			Sign
-																		</Button>
-																	</div>
-																)}
-															</Box>
-														</Section>
-													</div>
-												) : (
-													<img
-														className="popup_loader"
-														src="./img/loader.svg"
-														height={30}
-														width={30}
-														alt="loader"
-													/>
-												)}
-											</div>
-										)}
-									</Tile>
-								</Tile>
-							</Tile>
+								walletData={walletData}
+								onPopup={onPopup}
+								index={index}
+								containerItem={containerItem}
+								onGetObjectData={onGetObjectData}
+								onAuth={onAuth}
+								formatForContainerName={formatForContainerName}
+								onSetEacl={onSetEacl}
+							/>
 						))}
 						<Button
 							color="primary"
