79b66e49fa7e3b84d406b37942707d025322481a
Mikhail Petrov <mike@nspcc.ru>
2022-09-05 18:52:27 +0300

    [#19] Move functions to handle.js
    - formatForTreeView
    - formatForContainerName
    
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- src/Components/ContainerItem/ContainerItem.js
+++ src/Components/ContainerItem/ContainerItem.js
@@ -10,80 +10,11 @@ import {
 	Form,
 } from 'react-bulma-components';
 import TreeView from '../TreeView/TreeView';
+import {
+	formatForTreeView,
+	formatForContainerName,
+} from '../../Functions/handle';
 import api from '../../api';
-
-function formatForTreeView(objects) {
-	const getTreeView = objectsList => (
-		objectsList.reduce((root, item) => {
-			const parts = item.filePath ? item.filePath.split('/') : [''];
-			const lastPart = parts[parts.length - 1];
-			if (parts.length === 1 && lastPart === '') {
-				let childrenTemp = [];
-				if (root['/']) {
-					childrenTemp = root['/'];
-				}
-				root = { ...root, '/': [...childrenTemp, item] };
-			}
-			parts.filter((n) => n !== '').reduce((acc, part) => {
-				let children = [];
-				if (part === lastPart) {
-					let childrenTemp = [];
-					if (acc[part]) {
-						childrenTemp = acc[part]['/'];
-					}
-					children = [...childrenTemp, item];
-				} else if (acc[part]) {
-					let childrenTemp = [];
-					childrenTemp = acc[part]['/'];
-					children = [...childrenTemp];
-				}
-				return (acc[part] && (acc[part] = { ...acc[part], '/': children })) || (acc[part] = { '/': children });
-			}, root);
-			return root;
-		}, Object.create(null))
-	);
-
-	for (let i = 0; i < objects.length; i++) {
-		if (!objects[i].filePath) {
-			objects[i].filePath = '';
-		}
-
-		const path = objects[i].filePath.split('/');
-		if (path[path.length - 1] === '') {
-			objects[i].filePath = '';
-		} else {
-			objects[i].name = path[path.length - 1];
-			objects[i].filePath = path.slice(0, path.length - 1).join('/');
-		}
-
-		if (!objects[i].name) {
-			objects[i].name = objects[i].address.objectId;
-		}
-
-		objects[i].fullName = `${objects[i].filePath ? `${objects[i].filePath.trim()}/` : ''}${objects[i].name.trim()}`;
-	}
-
-	objects.sort((a, b) => {
-		if (a.fullName < b.fullName) {
-			return -1;
-		}
-
-		if (a.fullName > b.fullName) {
-			return 1;
-		}
-
-		return 0;
-	});
-	return getTreeView(objects);
-}
-
-function formatForContainerName(attributes, containerId) {
-	if (attributes.length > 0) {
-		const pos = attributes.map((item) => item.key).indexOf('Name');
-		return attributes[pos].value;
-	}
-	return containerId;
-}
 
 export default function ContainerItem({
 	containerItem,
---
+++ src/Functions/handle.js
@@ -0,0 +1,74 @@
+
+export function formatForTreeView(objects) {
+	const getTreeView = objectsList => (
+		objectsList.reduce((root, item) => {
+			const parts = item.filePath ? item.filePath.split('/') : [''];
+			const lastPart = parts[parts.length - 1];
+			if (parts.length === 1 && lastPart === '') {
+				let childrenTemp = [];
+				if (root['/']) {
+					childrenTemp = root['/'];
+				}
+				root = { ...root, '/': [...childrenTemp, item] };
+			}
+			parts.filter((n) => n !== '').reduce((acc, part) => {
+				let children = [];
+				if (part === lastPart) {
+					let childrenTemp = [];
+					if (acc[part]) {
+						childrenTemp = acc[part]['/'];
+					}
+					children = [...childrenTemp, item];
+				} else if (acc[part]) {
+					let childrenTemp = [];
+					childrenTemp = acc[part]['/'];
+					children = [...childrenTemp];
+				}
+				return (acc[part] && (acc[part] = { ...acc[part], '/': children })) || (acc[part] = { '/': children });
+			}, root);
+			return root;
+		}, Object.create(null))
+	);
+
+	for (let i = 0; i < objects.length; i++) {
+		if (!objects[i].filePath) {
+			objects[i].filePath = '';
+		}
+
+		const path = objects[i].filePath.replace(/\/+/g, '/').replace(/\\\\/g, '\\').split('/');
+		if (path[path.length - 1] === '') {
+			objects[i].filePath = '';
+		} else {
+			objects[i].name = path[path.length - 1]
+			objects[i].filePath = path.slice(0, path.length - 1).join('/');
+		}
+
+		if (!objects[i].name) {
+			objects[i].name = objects[i].address.objectId;
+		}
+
+		objects[i].fullName = `${objects[i].filePath ? `${objects[i].filePath.trim()}/` : ''}${objects[i].name.trim()}`;
+		console.log(objects[i])
+	}
+	
+	objects.sort((a, b) => {
+		if (a.fullName < b.fullName) {
+			return -1;
+		}
+
+		if (a.fullName > b.fullName) {
+			return 1;
+		}
+
+		return 0;
+	});
+	return getTreeView(objects);
+}
+
+export function formatForContainerName(attributes, containerId) {
+	if (attributes.length > 0) {
+		const pos = attributes.map((item) => item.key).indexOf('Name');
+		return attributes[pos].value;
+	}
+	return containerId;
+}
