31409d21e2f7c54476264f20d97ba90fcc431252
Mikhail Petrov <mike@nspcc.ru>
2022-10-17 12:59:15 +0300

    [#26] Add blob() method for response
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- src/api.js
+++ src/api.js
@@ -19,15 +19,21 @@ async function serverRequest(method, url, params, headers) {
 
 export default function api(method, url, params = {}, headers = {}) {
 	return new Promise((resolve) => {
-		serverRequest(method, url, params, headers).then(async (responce) => {
-			if (responce && responce.status === 204) {
+		serverRequest(method, url, params, headers).then(async (response) => {
+			if (response && response.status === 204) {
 				resolve({
 					status: 'success'
 				});
 			} else {
-				let res = responce;
-				res = await responce.json();
-				resolve(res);
+				let res = response;
+				if (method === 'GET' && url.indexOf(`${process.env.REACT_APP_HTTPGW}/get/`) !== -1) {
+					res = await response.blob();
+					const header = response.headers.get('Content-Type');
+					resolve({ header, res });
+				} else {
+					res = await response.json();
+					resolve(res);
+				}
 			}
 		});
 	});
