04b5ca1108e3e374d60da535ef4373f8c9be10be
Mikhail Petrov <mike@nspcc.ru>
2022-11-01 12:36:59 +0300

    [#67] Add warning for missing PROJECT_ID variable
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,8 @@ import {
 	Heading,
 	Button,
 	Box,
+	Tile,
+	Notification,
 } from 'react-bulma-components';
 
 const Home = ({
@@ -28,17 +30,31 @@ const Home = ({
 		>
 			<Section>
 				<Box style={{ maxWidth: 600, margin: 'auto' }}>
-					<Heading align="center">Connect your wallet to log in</Heading>
-					<Button
-						color="primary"
-						onClick={onConnectWallet}
-						style={{ display: 'flex', margin: '0 auto 20px' }}
-					>
-						Connect wallet
-					</Button>
-					<Heading align="center" size={6} weight="normal">
-						We support <a href="https://aero.coz.io/" target="_blank" rel="noopener noreferrer">Aero web wallet</a>, <a href="https://neonwallet.com/" target="_blank" rel="noopener noreferrer">NEON wallet</a>
-					</Heading>
+					{process.env.REACT_APP_WC_PROJECT_ID === '' ? (
+						<Tile kind="parent">
+							<Tile
+								kind="child"
+								renderAs={Notification}
+								color="danger"
+							>
+								Error: Global variable REACT_APP_WC_PROJECT_ID is not set. Obtain the <span style={{ fontWeight: 600 }}>WalletConnect project id</span> on the website <a href="https://cloud.walletconnect.com/app" style={{ textDecoration: 'underline' }} target="_blank" rel="noreferrer">cloud.walletconnect.com/app</a> and set in the .env file.
+							</Tile>
+						</Tile>
+					) : (
+						<>
+							<Heading align="center">Connect your wallet to log in</Heading>
+							<Button
+								color="primary"
+								onClick={onConnectWallet}
+								style={{ display: 'flex', margin: '0 auto 20px' }}
+							>
+								Connect wallet
+							</Button>
+							<Heading align="center" size={6} weight="normal">
+								We support <a href="https://aero.coz.io/" target="_blank" rel="noopener noreferrer">Aero web wallet</a>, <a href="https://neonwallet.com/" target="_blank" rel="noopener noreferrer">NEON wallet</a>
+							</Heading>
+						</>
+					)}
 				</Box>
 			</Section>
 		</Container>
